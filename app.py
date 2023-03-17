0cfaf496e4a242d5e61834d284e1961843f6a9bb
mdjorup <michael.djorup@gmail.com>
2023-03-17 14:41:35 -0400

    setting up environment variables

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+import json
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
 app = Flask(__name__)
 
+load_dotenv()
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Hello World"
+
+@app.route("/project")
+def project():
+    env_variable : str = os.environ.get("FIREBASE_SERVICE_ACCOUNT_CONFIG_JSON", "")
+    env_json = json.loads(env_variable)
+    return env_json.get("project_id", "No project id found")
 
 if __name__ == "__main__":
     app.run()
