13e078f36e3b10802a1870e6cb6464e2c1345f5a
mdjorup <michael.djorup@gmail.com>
2023-03-27 16:18:39 -0400

    organization blueprint notes

--- app/routes/organization_routes.py
+++ app/routes/organization_routes.py
@@ -2,4 +2,12 @@ from flask import Blueprint
 
 organization_blueprint = Blueprint("organization", __name__)
 
-
+# create organization -> need name, owner uid
+# delete an organization -> soft delete
+# add a coach -> owner must make the request, adds an existing uid to the
+# delete a coach -> owner must make the request, removes a uid from the coaches list
+# transfer owneership -> owner transfers ownership to a coach
+# add a player -> creates player object, adds it to the players subcollection
+# remove a player -> removes the player from the subcollection
+# create a team -> creates the team and adds it to the subcollection
+# Remove a team -> removes the team from the subcollection
